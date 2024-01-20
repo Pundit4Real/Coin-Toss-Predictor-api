e7c04824677abd6ab1c8d2574ec15f8057579a03
Mohammed Ali <mohammedaalli088@gmail.com>
2024-01-20 05:09:27 +0000

    feat:added the userProfile model

--- Api/models.py
+++ Api/models.py
@@ -3,4 +3,6 @@ from django.contrib.auth.models import User
 
 
 # user models.
-class
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delte=models.CASCADE)
+    balance = models.DecimalField(max_digits=0, decimal_places=2, default=0)
