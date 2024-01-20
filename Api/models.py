3cee1752a6862d3fb399c1bc5d1670823126f0d9
Mohammed Ali <mohammedaalli088@gmail.com>
2024-01-20 17:31:29 +0000

    ref: program codes refactored

--- Api/models.py
+++ Api/models.py
@@ -1,8 +1,1 @@
 from django.db import models
-from django.contrib.auth.models import User
-
-
-# user models.
-class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    balance = models.DecimalField(max_digits=10, decimal_places=2, default=0)
--- coinTossPredictor/settings.py
+++ coinTossPredictor/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework.authtoken',
     'rest_framework',
     'Api',
 ]
@@ -124,31 +123,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-# APPEND_SLASH = False
-
-# Configuring REST Framework Authentication and Parsing
-REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.TokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
-    ],
-    'DEFAULT_PARSER_CLASSES': (
-    'rest_framework.parsers.JSONParser',
-    'rest_framework.parsers.FormParser',
-    'rest_framework.parsers.MultiPartParser',
-    ),
-    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny',
-    ]
-}
-
-# # Configure authentication
-# AUTHENTICATION_BACKENDS = [
-#     'django.contrib.auth.backends.ModelBackend',
-# ]
-
-# # Configure session
-# SESSION_ENGINE = "django.contrib.sessions.backends.cached_db"
