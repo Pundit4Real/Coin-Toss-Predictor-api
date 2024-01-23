5e03df2bc9f6b33c1fe6e7587a2b1b35534a16fc
Mohammed Ali <mohammedaalli088@gmail.com>
2024-01-23 05:56:47 +0000

    ref: models.py refactored

--- Api/models.py
+++ Api/models.py
@@ -6,12 +6,14 @@ from django.db.models.signals import post_save
 
 class Prediction(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    timestamp = models.DateTimeField(default=timezone.now)
     side_predicted = models.CharField(max_length=4, choices=[('HEAD', 'HEAD'), ('TAIL', 'TAIL')])
     stake_amount = models.DecimalField(max_digits=10, decimal_places=2)
     result = models.CharField(max_length=4, choices=[('HEAD', 'HEAD'), ('TAIL', 'TAIL')])
+    timestamp = models.DateTimeField(default=timezone.now)
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    full_name = models.CharField(max_length=80)
     balance = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+
 
