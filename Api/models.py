2cdec3d1d2ae504fd25a20ce3eba6d5607d727c2
Mohammed Ali <mohammedaalli088@gmail.com>
2024-01-23 06:34:16 +0000

    ref: serializer.py file updated

--- Api/models.py
+++ Api/models.py
@@ -1,8 +1,25 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 from django.dispatch import receiver
 from django.utils import timezone
 from django.db.models.signals import post_save
+
+
+
+class CustomUser(AbstractUser):
+    full_name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.username
+    
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    balance = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+
+    def __str__(self):
+        return f"{self.user.username}'s Profile"
 
 class Prediction(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -11,9 +28,5 @@ class Prediction(models.Model):
     result = models.CharField(max_length=4, choices=[('HEAD', 'HEAD'), ('TAIL', 'TAIL')])
     timestamp = models.DateTimeField(default=timezone.now)
 
-class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    # full_name = models.CharField(max_length=80)
-    balance = models.DecimalField(max_digits=10, decimal_places=2, default=0)
 
 
--- Api/serializers.py
+++ Api/serializers.py
@@ -1,15 +1,28 @@
 from rest_framework import serializers
-from .models import UserProfile, Prediction
+from .models import UserProfile, Prediction, CustomUser
 
 class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserProfile
         fields = '__all__'
 
+class CustomUserSerializer(serializers.ModelSerializer):
+    profile = UserProfileSerializer()
+
+    class Meta:
+        model = CustomUser
+        fields = ['id', 'username', 'full_name', 'email', 'profile']
+
+    def create(self, validated_data):
+        profile_data = validated_data.pop('profile')
+        user = CustomUser.objects.create(**validated_data)
+        UserProfile.objects.create(user=user, **profile_data)
+        return user
 
 class PredictionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Prediction
         fields = ('timestamp', 'side_predicted', 'stake_amount', 'result')
+
 class BalanceUpdateSerializer(serializers.Serializer):
     balance = serializers.DecimalField(max_digits=10, decimal_places=2)
