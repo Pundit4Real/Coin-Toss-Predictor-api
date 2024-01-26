86f1db064211de8c0ddba648c77f623c1c2b816c
Mohammed Ali <mohammedaalli088@gmail.com>
2024-01-26 19:04:28 +0000

    ref:models refactored

--- Api/admin.py
+++ Api/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from .models import UserProfile, Prediction,User
 
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['username','email','first_name']
+
 # Register your models here.
 admin.site.register(UserProfile)
 admin.site.register(Prediction)
-admin.site.register(User)
+admin.site.register(User, UserAdmin)
+
+
--- coinTossPredictor/settings.py
+++ coinTossPredictor/settings.py
@@ -38,11 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Api', 
     'rest_framework',
     'rest_framework.authtoken',
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
-    'Api', 
 ]
 
 MIDDLEWARE = [
