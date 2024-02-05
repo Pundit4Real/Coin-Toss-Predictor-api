945475071b7630f9e065b264423e14d85c17544f
Mohammed Ali <mohammedaalli088@gmail.com>
2024-02-05 01:29:54 +0000

    ref: setting refactored

--- coinTossPredictor/settings.py
+++ coinTossPredictor/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'Api', 
     'rest_framework',
     'rest_framework.authtoken',
@@ -53,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'coinTossPredictor.urls'
@@ -149,3 +151,10 @@ SIMPLE_JWT = {
 }
 
 AUTH_USER_MODEL= 'Api.User'
+
+
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",  
+]
+
