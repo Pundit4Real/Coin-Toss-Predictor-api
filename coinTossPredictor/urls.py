0b110d441584da59b3da63f043d746191e3f892f
Mohammed Ali <mohammedaalli088@gmail.com>
2024-04-01 06:39:51 +0000

    ref: urls refactored

--- coinTossPredictor/urls.py
+++ coinTossPredictor/urls.py
@@ -30,9 +30,7 @@ schema_view = get_schema_view(
       title="ExpenseTracker Api",
       default_version='v1',
       description="Test description",
-      terms_of_service="https://www.ourapp.com/policies/terms/",
       contact=openapi.Contact(email="contact@expenses.local"),
-      license=openapi.License(name="Test License"),
    ),
    public=True,
    permission_classes=(permissions.AllowAny,),
@@ -44,9 +42,7 @@ urlpatterns = [
 
      #swagger urls
    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-   path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-   path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-   
+   path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),   
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root= settings.STATIC_ROOT)
