96c09a5f8da77a8ec754e14b9dd9d2a0774bec07
Mohammed <mohammedaalli088@gmail.com>
2024-09-20 22:17:05 +0000

    ref: settings updated

--- coinTossPredictor/settings.py
+++ coinTossPredictor/settings.py
@@ -205,5 +205,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'test123.moali@gmail.com'
-EMAIL_HOST_PASSWORD = 'zquzxxqleoptctif'
+EMAIL_HOST_PASSWORD = 'bevnfpjwkmodnuln'
 
