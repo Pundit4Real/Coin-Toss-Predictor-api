251cef932f6fdac1aa9991b365dadc9e74ea13e1
Mohammed <mohammedaalli088@gmail.com>
2024-09-26 21:52:44 +0000

    ref:settings file updated

--- coinTossPredictor/settings.py
+++ coinTossPredictor/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-bt+x%n(nnfkf%&^+0uygdej^y@=&_)kqp_*yp8pi@juwl4=s8+
 DEBUG = True
 
 
-ALLOWED_HOSTS = ['127.0.0.1','localhost','CoinTossApi.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1','localhost','cointossproject.pythonanywhere.com']
 
 # Application definition
 
